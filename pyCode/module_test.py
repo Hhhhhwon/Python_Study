94f1036628e41f776a3f510709d28ef872b17bbc
KimHwiWon <hwkim@bizarim.com>
2024-04-03 19:30:25 +0900

    push

---
+++ pyCode/module_test.py
@@ -0,0 +1,24 @@
+# 모듈 사용 실습
+
+import sys
+
+print(sys)
+
+print(sys.path)
+
+print(type(sys.path))
+
+# 모듈 경로 삽입
+sys.path.append('C:/math')
+
+print(sys.path)
+import test
+
+# 모듈 사용
+print(test_module.power(10, 3))
+
+import chapter06_02
+
+
+
+print(chapter06_02.add(3,5))
